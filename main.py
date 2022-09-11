cb38ba121f570711bb26d7e6b80eb13e23b37d04
andyvy <thienvy95@gmail.com>
2022-09-12 06:19:54 +0700

    create class Paddle

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from turtle import Screen
+from paddle import Paddle
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
+while game_is_on:
+    screen.update()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()
